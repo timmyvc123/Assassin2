ddbc875aa264af17975c11a48c2c1a15819336a7
Tim <timmyvc123@gmail.com>
2018-10-30 15:31:11 -0400

    Initial Commit

---
+++ Assassin2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assassin2
+//
+//  Created by Timmy Van Cauwenberge on 10/30/18.
+//  Copyright © 2018 Cowabunga Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
